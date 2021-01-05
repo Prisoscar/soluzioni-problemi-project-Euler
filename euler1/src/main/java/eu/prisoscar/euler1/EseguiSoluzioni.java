7c8efb83c08541f983d7bf0cc8152e16373b0dad
Prisoscar <67907720+Prisoscar@users.noreply.github.com>
2021-01-05 20:30:11 +0100

    da rifare

--- euler1/src/main/java/eu/prisoscar/euler1/EseguiSoluzioni.java
+++ euler1/src/main/java/eu/prisoscar/euler1/EseguiSoluzioni.java
@@ -3,17 +3,22 @@ package eu.prisoscar.euler1;
 import eu.prisoscar.euler1.algoritmi_trasversali.NumeriPrimi;
 import eu.prisoscar.euler1.soluzioni.Prbl_1_Multiples_3_5;
 import eu.prisoscar.euler1.soluzioni.Prbl_49_PrimePermutations;
+import eu.prisoscar.euler1.soluzioni.Probl_60_PrimePairSets;
+import java.util.List;
+import java.util.Map;
 
 public class EseguiSoluzioni {
     public static void main(String[] args) {
         //problema 1 https://projecteuler.net/problem=1
         System.out.println("La soluzione del problema 1 è: " + Prbl_1_Multiples_3_5.sommaMultipli());
         
-        System.out.println(NumeriPrimi.trovaPrimi(1000000000) + "\n\n" + NumeriPrimi.trovaPrimi(10000000).size());
+        //System.out.println(NumeriPrimi.trovaPrimi(1000000000) + "\n\n" + NumeriPrimi.trovaPrimi(10000000).size());
         //problema 49 https://projecteuler.net/problem=49
         System.out.println("La soluzione del problema 49 è: " + Prbl_49_PrimePermutations.permutation());
         
         //problema 60 https://projecteuler.net/problem=60
+        Map.Entry<Long, List<Long>> solution = Probl_60_PrimePairSets.sumPrimeSet();
+        System.out.println("La soluzione del problema 60 è: " + solution.getKey() + " ottenuta dai segurnti primi: " + solution.getValue());
         
     }
     
--- euler1/src/main/java/eu/prisoscar/euler1/soluzioni/Probl_60_PrimePairSets.java
+++ euler1/src/main/java/eu/prisoscar/euler1/soluzioni/Probl_60_PrimePairSets.java
@@ -1,7 +1,11 @@
 package eu.prisoscar.euler1.soluzioni;
 
 import eu.prisoscar.euler1.algoritmi_trasversali.NumeriPrimi;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /*
     Problem 60 https://projecteuler.net/problem=60
@@ -10,18 +14,59 @@ import java.util.List;
     The sum of these four primes, 792, represents the lowest sum for a set of four primes with this property.
 
     Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
-*/
+ */
 public class Probl_60_PrimePairSets {
-    public static Integer sumPrimeSet (){
-        List<Long> primiAnalizzati = NumeriPrimi.trovaPrimi(1000000);
-        
-        for (int i = 1; primiAnalizzati.get(i) < 10000; i++) {
-            
+
+    public static Entry<Long, List<Long>> sumPrimeSet() {
+        List<Long> analizedPrimes = NumeriPrimi.trovaPrimi(100000000_0);
+        List<Long> primeSet = new ArrayList<>();
+        Map<Long, List<Long>> primeSets = new HashMap<>();
+        boolean areConcatenationsPrimes;
+        long setSum = Long.MAX_VALUE;
+        //String primeSetString;
+        Entry<Long, List<Long>> solution = null;
+        long skipped = 0L;
+
+        for (int i = 1; analizedPrimes.get(i) < 100L; i++) {
+            skipped = analizedPrimes.get(i + 1);
+            for (int j = i + 1; analizedPrimes.get(j) < 100L; j = analizedPrimes.indexOf(skipped)) {
+                primeSet.clear();
+                primeSet.add(analizedPrimes.get(i));
+                for (int k = j + 1; k < analizedPrimes.size() && primeSet.size() <= 5 && analizedPrimes.get(k) < 10000_0L; k++) {
+                    areConcatenationsPrimes = true;
+                    for (Long primo : primeSet) {
+                        if (!areConcatenationsPrime(primo, analizedPrimes.get(k), analizedPrimes)) {
+                            areConcatenationsPrimes = false;
+                            break;
+                        }
+                    }
+                    if (areConcatenationsPrimes) {
+                        primeSet.add(analizedPrimes.get(k));
+                    }
+                }
+                System.out.println(primeSet);
+                if (primeSet.size() == 5) {
+                    setSum = primeSet.stream().map(primo -> primo).reduce(setSum, (accumulator, _item) -> accumulator + _item);     //dovrebbe sommare tutti gli elementi di primeSet
+                    primeSets.put(setSum, primeSet);
+                }
+                try {
+                    skipped = primeSet.get(1);
+                } catch (IndexOutOfBoundsException ex) {
+                    break;
+                }
+            }
         }
-        return null;
+        for (Entry<Long, List<Long>> entryPrimeSets : primeSets.entrySet()) {
+            if (entryPrimeSets.getKey() < setSum) {
+                setSum = entryPrimeSets.getKey();
+                //primeSetString = entryPrimeSets.getValue().toString();
+                solution = entryPrimeSets;
+            }
+        }
+        return solution;
     }
-    
-    public static boolean areConcatenationsPrime (long num1, long num2, List<Long> primiAnalizati){
+
+    public static boolean areConcatenationsPrime(long num1, long num2, List<Long> primiAnalizati) {
         return primiAnalizati.contains(Long.parseLong(String.valueOf(num1).concat(String.valueOf(num2)))) && primiAnalizati.contains(Long.parseLong(String.valueOf(num2).concat(String.valueOf(num1))));
     }
 }
