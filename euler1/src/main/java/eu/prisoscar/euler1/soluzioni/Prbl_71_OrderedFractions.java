427e297c9e961c7baf3718a1ec5754981cba29b0
Mihail <67907720+Prisoscar@users.noreply.github.com>
2022-01-06 14:14:51 +0100

    solved prbl 71

--- euler1/src/main/java/eu/prisoscar/euler1/soluzioni/Prbl_71_OrderedFractions.java
+++ euler1/src/main/java/eu/prisoscar/euler1/soluzioni/Prbl_71_OrderedFractions.java
@@ -10,25 +10,30 @@ public class Prbl_71_OrderedFractions {
     public static int getSolution(){
         int targetNumerator = 2;
         double lastLeftFractionValue = 2D / 5;
-        //i = denominator
+        long elapsedTime = System.currentTimeMillis();
+        System.out.println("Problem 71:");
         for (int i = 2; i <= TARGET; i++){
-            //j = numerator
+            if (i % 100_000 == 0){
+                System.out.println(i / 100_000 * 10 + "%");
+            }
             double fractionValue;
-            System.out.println(i);
             for (int j = 1; j < i; j++){
                 //try all fraction combination, if the fraction result
                 //is suitable for being the value at left of 3/7
                 //will check if is effectively a reduced proper fraction
                 fractionValue = (double)j / i;
-                if (fractionValue > TARGET_FRACTION_VALUE) break;
+                if (fractionValue >= TARGET_FRACTION_VALUE) break;
                 if (fractionValue > lastLeftFractionValue) {
                     if (PrimeNumbers.areCoprimes(i, j)) {
                         targetNumerator = j;
                         lastLeftFractionValue = fractionValue;
+                        //System.out.println(targetNumerator + " over " + targetDenominator);
                     }
                 }
             }
         }
+        double elapsedMinutes = (System.currentTimeMillis() - elapsedTime) / 60000D;
+        System.out.println("Problem 70 solved in " + elapsedMinutes + " minutes!");
         return targetNumerator;
     }
 }
